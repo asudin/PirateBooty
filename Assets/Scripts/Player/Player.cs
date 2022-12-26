1fd521e35e1f3a7a90b4d72c646606f9cb8c9240
asudin <artem.sudin@gmail.com>
2022-12-26 13:12:07 +0300

    Weapon shooting
    - Currently picked weapon can now shoot projectiles.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,6 +10,12 @@ public class Player : MonoBehaviour
     [SerializeField] private Animator _animator;
     [SerializeField] private AudioSource _dieSound;
 
+    [Header("Current Weapon")]
+    [SerializeField] private Transform _weaponParent;
+    [SerializeField] private Weapon _currentWeapon;
+
+    private float _lastShotTime;
+
     private void OnEnable() => _coin.Collected += ChangeCountWallet;
 
     private void OnDisable() => _coin.Collected -= ChangeCountWallet;
@@ -18,6 +24,28 @@ public class Player : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _dieSound = GetComponent<AudioSource>();
+        _currentWeapon = InstantiateWeapon();
+    }
+
+    private void Update()
+    {
+        PlayerShoot();
+    }
+
+    private Weapon InstantiateWeapon()
+    {
+        return Instantiate(_currentWeapon, _weaponParent.position, transform.rotation, _weaponParent);
+    }
+
+    private void PlayerShoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+            if (_lastShotTime <= 0)
+            {
+                _currentWeapon.Shoot();
+                _lastShotTime = _currentWeapon.ShootingCooldown;
+            }
+        _lastShotTime -= Time.deltaTime;
     }
 
     private void ChangeCountWallet(int score)
