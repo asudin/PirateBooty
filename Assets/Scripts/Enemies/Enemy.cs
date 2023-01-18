85c5ea7805a9a1c523792a3b491c06e37f5ff09b
asudin <artem.sudin@gmail.com>
2023-01-18 22:45:03 +0300

    Enraged enemy fix
    Fixed a collision bug with the enraged enemy type.

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private EnemyData _enemyData;
+    [SerializeField] private EnemyVariant _variant;
 
     private EnemyCollisionHandler _collisionHandler;
 
     public EnemyCollisionHandler CollisionHandler => _collisionHandler;
     public EnemyData EnemyData => _enemyData;
+    public EnemyVariant Variant => _variant;
 
     private void Awake()
     {
         _collisionHandler = GetComponent<EnemyCollisionHandler>();
     }
+}
+
+[Serializable]
+public class EnemyVariant
+{
+    public EnemyType Type;
+}
+
+public enum EnemyType
+{
+    Normal,
+    Enraged
 }
 
