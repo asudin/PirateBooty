094ba90b4fe155d5b705edebc194ca83578c2995
asudin <artem.sudin@gmail.com>
2023-01-22 22:32:20 +0300

    Bullet type collision
    Fixed bullet collision missing animation, VFX and depending on bullet type the destroy animation.

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -1,40 +1,61 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
     [Header("Configurations")]
+    [SerializeField] private Rigidbody2D _bulletRigidbody;
+    [SerializeField] private BulletVariant _bulletVariant;
     [SerializeField] private int _damage;
     [SerializeField] private float _speed;
 
     [Header("Effects")]
     [SerializeField] private Animator _animator;
 
+    private void Awake()
+    {
+        _bulletRigidbody = GetComponent<Rigidbody2D>();
+    }
+
     public void Move()
     {
         transform.Translate(Vector2.right * _speed * Time.deltaTime);
     }
 
-    public void DestroyBullet()
+    private void DestroyBullet()
     {
-        var destroyTimerDelay = 0.2f;
+        if (_bulletVariant.Type == BulletType.Shotgun)
+            _bulletRigidbody.bodyType = RigidbodyType2D.Kinematic;
 
-        //_animator.SetTrigger("isDestroyed");
+        var destroyTimerDelay = 0.2f;
+        _speed = 0f;
+        _animator.SetTrigger("isDestroyed");
         Destroy(gameObject, destroyTimerDelay);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Enemy enemy))
-        {
             DestroyBullet();
-        }
 
         if (collision.gameObject.TryGetComponent(out Wall wall) ||
             collision.gameObject.TryGetComponent(out Ground ground))
-        {
             DestroyBullet();
-        }
     }
+}
+
+[Serializable]
+public class BulletVariant
+{
+    public BulletType Type;
+}
+
+public enum BulletType
+{
+    Rifle,
+    Cannon,
+    Shotgun
 }
--- Assets/Scripts/Weapons/Shotgun/ShotgunBullet.cs
+++ Assets/Scripts/Weapons/Shotgun/ShotgunBullet.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class ShotgunBullet : Bullet
 {
     [SerializeField] private float destroyTimerDelay;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
