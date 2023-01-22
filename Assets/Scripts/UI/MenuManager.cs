8b20f0571c869ab754e97dc68cc5f8a66d948a4c
asudin <artem.sudin@gmail.com>
2023-01-22 15:26:28 +0300

    Start screen dark effect fix
    Fixed the dark effect on the start screen.

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -6,15 +6,17 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private CanvasGroup _startPanel;
+    [SerializeField] private CanvasGroup _shadowPanel;
     [SerializeField] private SoundManager _sounds;
-    [SerializeField, Tooltip("Higher positive value equals faster fade in/out animation.")] private float _time;
+    [SerializeField, Tooltip("Higher positive value equals faster fade in/out animation.")] private float _fadingTime;
 
     private SoundManager _soundManager;
 
     private void Start()
     {
         DontDestroyOnLoad(_sounds);
-        StartCoroutine(_startPanel.FadeIn(_time));
+        StartCoroutine(_startPanel.FadeIn(_fadingTime));
+        StartCoroutine(_shadowPanel.FadeOut(_fadingTime));
         _soundManager = ServiceLocator.Get<SoundManager>();
     }
 
